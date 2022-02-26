c1c5ceee40fcd3d2ec5346300191e33df9f8c341
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2022-02-26 16:07:29 +0200

    Cleanup offer param encoding

--- offers.go
+++ offers.go
@@ -4,20 +4,21 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 type (
 	OfferClient interface {
-		UpdateOfferPassenger(ctx context.Context, input *PassengerUpdateInput) (*Passenger, error)
-		// GetOffer(ctx context.Context, id string) (*Offer, error)
+		UpdateOfferPassenger(ctx context.Context, offerRequestID, passengerID string, input *PassengerUpdateInput) (*Passenger, error)
 		ListOffers(ctx context.Context, reqId string, options ...ListOffersParams) *Iter[Offer]
+		GetOffer(ctx context.Context, id string) (*Offer, error)
 	}
 
 	ListOffersSortParam string
 
 	ListOffersParams struct {
-		Sort           string `json:"sort"`
-		MaxConnections int    `json:"max_connections"`
+		Sort           ListOffersSortParam `url:"sort,omitempty"`
+		MaxConnections int                 `url:"max_connections,omiempty"`
 	}
 )
 
@@ -35,18 +36,24 @@ func (a *API) UpdateOfferPassenger(ctx context.Context, offerRequestID, passenge
 
 // ListOffers lists all the offers for an offer request. Returns an iterator.
 func (a *API) ListOffers(ctx context.Context, offerRequestId string, options ...ListOffersParams) *Iter[Offer] {
+	if offerRequestId == "" {
+		return GetIter(func(meta *ListMeta) (*List[Offer], error) {
+			return nil, fmt.Errorf("offerRequestId param is required")
+		})
+	} else if !strings.HasPrefix(offerRequestId, "orq_") {
+		return GetIter(func(meta *ListMeta) (*List[Offer], error) {
+			return nil, fmt.Errorf("offerRequestId should begin with orq_")
+		})
+	}
+
 	c := newInternalClient[struct{}, Offer](a)
-	return c.getIterator(ctx, http.MethodGet, "/air/offers", func(req *http.Request) {
-		q := req.URL.Query()
-		q.Add("offer_request_id", offerRequestId)
-		if len(options) == 1 {
-			if options[0].Sort != "" {
-				q.Add("sort", string(options[0].Sort))
-			}
-			if options[0].MaxConnections != 0 {
-				q.Add("max_connections", fmt.Sprintf("%d", options[0].MaxConnections))
-			}
-		}
-		req.URL.RawQuery = q.Encode()
-	})
+	return c.getIterator(ctx, http.MethodGet, "/air/offers",
+		WithURLParam("offer_request_id", offerRequestId),
+		WithURLParams(options...))
+}
+
+// GetOffer gets a single offer by ID.
+func (a *API) GetOffer(ctx context.Context, id string) (*Offer, error) {
+	c := newInternalClient[struct{}, Offer](a)
+	return c.makeRequestWithPayload(ctx, "/air/offers/"+id, http.MethodGet, nil)
 }

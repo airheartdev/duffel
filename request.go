79b47d38d9fe78768c1673a33d0c235a8f9a6fd8
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2022-02-28 15:09:58 +0200

    Add fluent based request builder

--- request.go
+++ request.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/segmentio/encoding/json"
 
-	"github.com/gorilla/schema"
 	"github.com/pkg/errors"
 	"golang.org/x/time/rate"
 )
@@ -25,51 +24,6 @@ func newInternalClient[Req any, Resp any](a *API) *client[Req, Resp] {
 func (c *client[R, W]) Debug() *client[R, W] {
 	c.debug = true
 	return c
-}
-
-// Get makes a GET request to the specified resource.
-func (c *client[Req, Resp]) Get(ctx context.Context, resourcePath string, opts ...RequestOption) (*http.Response, error) {
-	err := c.limiter.Wait(ctx) // This is a blocking call. Honors the rate limit
-	if err != nil {
-		return nil, err
-	}
-
-	resp, err := c.makeRequest(ctx, resourcePath, http.MethodGet, nil, opts...)
-	if err != nil {
-		return nil, err
-	}
-
-	rateLimit, err := parseRateLimit(resp)
-	if err != nil {
-		return nil, err
-	}
-
-	c.rateLimit = rateLimit
-	c.limiter.SetBurst(rateLimit.Limit)
-	c.limiter.SetLimit(rate.Every(rateLimit.Period))
-
-	if rateLimit.Remaining == 0 || resp.StatusCode == http.StatusTooManyRequests {
-		return nil, fmt.Errorf("rate limit exceeded, reset in: %s, current limit: %d", rateLimit.Period.String(), rateLimit.Limit)
-	}
-	return resp, nil
-}
-
-func (c *client[Req, Resp]) Post(ctx context.Context, resourcePath string, requestInput *Req, opts ...RequestOption) (*Resp, error) {
-	resp, err := c.makeIteratorRequest(ctx, resourcePath, http.MethodPost, requestInput, opts...)
-	if err != nil {
-		return nil, err
-	}
-
-	return &resp.Data, nil
-}
-
-func (c *client[Req, Resp]) makeRequestWithPayload(ctx context.Context, resourceName string, method string, requestInput *Req, requestBuilders ...RequestOption) (*Resp, error) {
-	resp, err := c.makeIteratorRequest(ctx, resourceName, method, requestInput, requestBuilders...)
-	if err != nil {
-		return nil, err
-	}
-
-	return &resp.Data, nil
 }
 
 func (c *client[Req, Resp]) Do(ctx context.Context, resourceName string, method string, body *Req, opts ...RequestOption) (*http.Response, error) {
@@ -103,6 +57,7 @@ func (c *client[Req, Resp]) Do(ctx context.Context, resourceName string, method 
 	return resp, nil
 }
 
+// DEPRECATED
 func (c *client[Req, Resp]) makeIteratorRequest(ctx context.Context, resourceName string, method string, requestInput *Req, requestBuilders ...RequestOption) (*ResponsePayload[Resp], error) {
 	resp, err := c.Do(ctx, resourceName, method, requestInput, requestBuilders...)
 	if err != nil {
@@ -124,6 +79,7 @@ func (c *client[Req, Resp]) makeIteratorRequest(ctx context.Context, resourceNam
 	return container, nil
 }
 
+// DEPRECATED
 func (c *client[Req, Resp]) makeListRequest(ctx context.Context, resourceName string, method string, requestInput *Req, requestBuilders ...RequestOption) (*ResponsePayload[[]*Resp], error) {
 	resp, err := c.Do(ctx, resourceName, method, requestInput, requestBuilders...)
 	if err != nil {
@@ -145,6 +101,7 @@ func (c *client[Req, Resp]) makeListRequest(ctx context.Context, resourceName st
 	return container, nil
 }
 
+// DEPRECATED
 func (c *client[Req, Resp]) getIterator(ctx context.Context, method string, resourceName string, requestBuilders ...RequestOption) *Iter[Resp] {
 	return GetIter(func(lastMeta *ListMeta) (*List[Resp], error) {
 		list := new(List[Resp])
@@ -161,45 +118,12 @@ func (c *client[Req, Resp]) getIterator(ctx context.Context, method string, reso
 	})
 }
 
-func WithRequestPagination(meta *ListMeta) RequestOption {
-	return func(req *http.Request) error {
-		q := req.URL.Query()
-
-		if meta != nil {
-			enc := schema.NewEncoder()
-			enc.SetAliasTag("url")
-			err := enc.Encode(meta, q)
-			if err != nil {
-				return (err)
-			}
-			req.URL.RawQuery = q.Encode()
-		}
-		return nil
+// DEPRECATED
+func (c *client[Req, Resp]) makeRequestWithPayload(ctx context.Context, resourceName string, method string, requestInput *Req, requestBuilders ...RequestOption) (*Resp, error) {
+	resp, err := c.makeIteratorRequest(ctx, resourceName, method, requestInput, requestBuilders...)
+	if err != nil {
+		return nil, err
 	}
-}
 
-func WithURLParams[T any](params ...T) RequestOption {
-	return func(req *http.Request) error {
-		q := req.URL.Query()
-		enc := schema.NewEncoder()
-		enc.SetAliasTag("url")
-
-		for _, param := range params {
-			err := enc.Encode(param, q)
-			if err != nil {
-				return (err)
-			}
-		}
-		req.URL.RawQuery = q.Encode()
-		return nil
-	}
-}
-
-func WithURLParam(key, value string) RequestOption {
-	return func(req *http.Request) error {
-		q := req.URL.Query()
-		q.Add(key, value)
-		req.URL.RawQuery = q.Encode()
-		return nil
-	}
+	return &resp.Data, nil
 }
---
+++ requestbuilder.go
@@ -0,0 +1,212 @@
+package duffel
+
+import (
+	"context"
+	"net/http"
+	"net/url"
+
+	"github.com/gorilla/schema"
+	"github.com/pkg/errors"
+	"github.com/segmentio/encoding/json"
+)
+
+type RequestBuilder[Req any, Resp any] struct {
+	client         *client[Req, Resp]
+	method         string
+	host           string
+	resourcePath   string
+	requestOptions []RequestOption
+	body           *Req
+	params         url.Values
+}
+
+type RequestMiddleware func(r *http.Request) error
+type ResponseMiddleware func(r *http.Response) error
+
+type ParamEncoder[T any] interface {
+	Encode(v url.Values) error
+}
+
+func WithRequestPagination(meta *ListMeta) RequestOption {
+	return func(req *http.Request) error {
+		q := req.URL.Query()
+
+		if meta != nil {
+			enc := schema.NewEncoder()
+			enc.SetAliasTag("url")
+			err := enc.Encode(meta, q)
+			if err != nil {
+				return (err)
+			}
+			req.URL.RawQuery = q.Encode()
+		}
+		return nil
+	}
+}
+
+func WithEncodableParams[T any](params ...ParamEncoder[T]) RequestOption {
+	return func(req *http.Request) error {
+		q := req.URL.Query()
+
+		for _, param := range params {
+			err := param.Encode(q)
+			if err != nil {
+				return err
+			}
+		}
+
+		req.URL.RawQuery = q.Encode()
+		return nil
+	}
+}
+
+func WithURLParams[T any](params ...T) RequestOption {
+	return func(req *http.Request) error {
+		q := req.URL.Query()
+		enc := schema.NewEncoder()
+		enc.SetAliasTag("url")
+
+		for _, param := range params {
+			err := enc.Encode(param, q)
+			if err != nil {
+				return err
+			}
+		}
+		req.URL.RawQuery = q.Encode()
+		return nil
+	}
+}
+
+func WithURLParam(key, value string) RequestOption {
+	return func(req *http.Request) error {
+		q := req.URL.Query()
+		q.Add(key, value)
+		req.URL.RawQuery = q.Encode()
+		return nil
+	}
+}
+
+// NewRequestWithAPI returns a new fluent requst builder for the given API.
+// The input types are the request payload and response payload.
+// For Get requests, the request payload is used to type the URL params.
+//
+// The response payload is used to type the parsed response body, and is the type returned by the finalizers.
+func NewRequestWithAPI[ReqT any, ResponseT any](a *API) *RequestBuilder[ReqT, ResponseT] {
+	return &RequestBuilder[ReqT, ResponseT]{
+		client: newInternalClient[ReqT, ResponseT](a),
+		host:   defaultHost,
+		method: http.MethodGet,
+	}
+}
+
+// Host sets the host to an alternate host to duffel.DefaultHost
+func (r *RequestBuilder[Req, Resp]) Host(host string) *RequestBuilder[Req, Resp] {
+	r.host = host
+	return r
+}
+
+// WithParam adds a single query param to the URL.
+// These operations will be applied in defined order after the request is initialized.
+func (r *RequestBuilder[Req, Resp]) WithParam(key, value string) *RequestBuilder[Req, Resp] {
+	r.requestOptions = append(r.requestOptions, WithURLParam(key, value))
+	return r
+}
+
+// WithParams sets the URL query params for the request.
+// These operations will be applied in defined order after the request is initialized.
+func (r *RequestBuilder[Req, Resp]) WithParams(obj ...ParamEncoder[Req]) *RequestBuilder[Req, Resp] {
+	r.requestOptions = append(r.requestOptions, WithEncodableParams(obj...))
+	return r
+}
+
+func (r *RequestBuilder[Req, Resp]) WithOptions(opts ...RequestOption) *RequestBuilder[Req, Resp] {
+	r.requestOptions = append(r.requestOptions, opts...)
+	return r
+}
+
+// Get sets the request method to GET and the request path to the given path. Global request options are applied.
+func (r *RequestBuilder[Req, Resp]) Get(path string, opts ...RequestOption) *RequestBuilder[Req, Resp] {
+	r.method = http.MethodGet
+	r.resourcePath = path
+	r.requestOptions = append(r.requestOptions, opts...)
+	return r
+}
+
+// Post sets the request method to POST, the request path to the given path, and the request payload to body. Global request options are applied.
+func (r *RequestBuilder[Req, Resp]) Post(path string, body *Req, opts ...RequestOption) *RequestBuilder[Req, Resp] {
+	r.method = http.MethodPost
+	r.resourcePath = path
+	r.requestOptions = append(r.requestOptions, opts...)
+	r.body = body
+	return r
+}
+
+// Patch sets the request method to PATCH, the request path to the given path, and the request payload to body. Global request options are applied.
+func (r *RequestBuilder[Req, Resp]) Patch(path string, body *Req, opts ...RequestOption) *RequestBuilder[Req, Resp] {
+	r.method = http.MethodPatch
+	r.resourcePath = path
+	r.requestOptions = append(r.requestOptions, opts...)
+	r.body = body
+	return r
+}
+
+// All finalizes the request and returns an iterator over the response.
+func (r *RequestBuilder[Req, Resp]) All(ctx context.Context) *Iter[Resp] {
+	return GetIter(func(lastMeta *ListMeta) (*List[Resp], error) {
+		list := new(List[Resp])
+		response, err := r.makeRequest(ctx, WithRequestPagination(lastMeta))
+		if err != nil {
+			return nil, err
+		}
+		container := new(ResponsePayload[[]*Resp])
+
+		err = decodeResponse(response, &container)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to decode response")
+		}
+
+		list.SetListMeta(container.Meta)
+		list.SetItems(container.Data)
+		return list, nil
+	})
+}
+
+// One finalizes the request and returns a single item response.
+func (r *RequestBuilder[Req, Resp]) One(ctx context.Context) (*Resp, error) {
+	response, err := r.makeRequest(ctx)
+	if err != nil {
+		return nil, err
+	}
+	container := new(ResponsePayload[*Resp])
+	err = decodeResponse(response, &container)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to decode response")
+	}
+
+	return container.Data, nil
+}
+
+func (r *RequestBuilder[Req, Resp]) makeRequest(ctx context.Context, opts ...RequestOption) (*http.Response, error) {
+	return r.client.Do(ctx, r.resourcePath, r.method, r.body, append(r.requestOptions, opts...)...)
+}
+
+func decodeResponse[T any](resp *http.Response, v T) error {
+	reader, err := gzipResponseReader(resp)
+	if err != nil {
+		return err
+	}
+	defer reader.Close()
+
+	return json.NewDecoder(reader).Decode(v)
+}
+
+// normalizeParams returns a slice of interfaces from the given params.
+// This is only neeeded because Go doesn't allow slice conversion of slice spreads.
+// See: https://github.com/golang/go/wiki/InterfaceSlice
+func normalizeParams[T ParamEncoder[T]](params []T) []ParamEncoder[T] {
+	p := make([]ParamEncoder[T], len(params))
+	for i, param := range params {
+		p[i] = ParamEncoder[T](param)
+	}
+	return p
+}

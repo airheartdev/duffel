3b240ed842f54b4d3094e6f4d10efdf997e79391
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2022-06-13 11:29:07 -0500

    Update license header

--- scripts/license.sh
+++ scripts/license.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 # Copyright 2021-present Airheart, Inc. All rights reserved.
 # This source code is licensed under the Apache 2.0 license found
 # in the LICENSE file in the root directory of this source tree.

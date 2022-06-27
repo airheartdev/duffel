2a44f4cf625a2b0ad3ab585fd12d59e735a41c84
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2022-06-27 12:05:15 +1000

    Fixing Seatmap support

--- seatmaps_test.go
+++ seatmaps_test.go
@@ -30,21 +30,18 @@ func TestGetSeatmaps(t *testing.T) {
 	ctx := context.TODO()
 
 	client := New("duffel_test_123")
-	iter := client.GetSeatmaps(ctx, "off_00009htYpSCXrwaB9DnUm0")
-
-	iter.Next()
-	data := iter.Current()
-	err := iter.Err()
-	reqID, _ := iter.LastRequestID()
-
+	seats, err := client.GetSeatmap(ctx, "off_00009htYpSCXrwaB9DnUm0")
 	a.NoError(err)
-	a.NotNil(data)
+	reqID, _ := client.LastRequestID()
+
+	a.NotNil(seats)
 	a.Equal("FvxRwfnMtKgc0EwCCoXE", reqID)
 
-	a.Equal("sea_00003hthlsHZ8W4LxXjkzo", data.ID)
-	a.Equal("seg_00009htYpSCXrwaB9Dn456", data.SegmentID)
-	a.Equal("sli_00009htYpSCXrwaB9Dn123", data.SliceID)
+	seat := seats[0]
+	a.Equal("sea_00003hthlsHZ8W4LxXjkzo", seat.ID)
+	a.Equal("seg_00009htYpSCXrwaB9Dn456", seat.SegmentID)
+	a.Equal("sli_00009htYpSCXrwaB9Dn123", seat.SliceID)
 
-	serviceAmount := data.Cabins[0].Rows[0].Sections[0].Elements[0].AvailableServices[0].TotalAmount().String()
+	serviceAmount := seat.Cabins[0].Rows[0].Sections[0].Elements[0].AvailableServices[0].TotalAmount().String()
 	a.Equal("30.00 GBP", serviceAmount)
 }
